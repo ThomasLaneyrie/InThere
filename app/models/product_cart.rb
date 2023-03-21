ac97234a0ce8c69a5717a316d34e57048bb0c3a3
ThomasLaneyrie <thomas.laneyrie@gmail.com>
2023-03-21 15:22:14 +0100

    en-cours

--- app/models/product_cart.rb
+++ app/models/product_cart.rb
@@ -3,4 +3,20 @@ class ProductCart < ApplicationRecord
   belongs_to :product
 
   validates :quantity, presence: true, numericality: { greater_than: 0 }
+
+  def add_quantity
+    @line_cart = LineCart.find(params[:id])
+    @line_cart.quantity += 1
+    @line_cart.save
+    redirect_to panier_path(@line_cart.cart)
+  end
+
+  def reduce_quantity
+    @line_cart = LineCart.find(params[:id])
+    if @line_cart.quantity > 1 
+      @line_cart.quantity -= 1
+      @line_cart.save
+    end
+    redirect_to panier_path(@line_cart.cart)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  resources :products
-  get 'users/show'
-  get 'users/edit'
-  get 'users/update'
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -11,5 +6,16 @@ Rails.application.routes.draw do
 
   root to: "static_pages#landing_page"  
   devise_for :users
+  resources :users, only: [:show, :destroy]
+  resources :products
+
+  # Routes relatives aux product_carts
+  resources :product_carts, only: [:create, :destroy]
+  get 'product_carts/:id/add' => "product_carts#add_quantity", as: "product_cart_add"
+  get 'product_carts/:id/reduce' => "product_carts#reduce_quantity", as: "product_cart_reduce"
+
+  # Routes relatives aux carts
+  get 'carts/:id', to: 'carts#show', as: 'panier'
+  resources :carts, only: [:show]
 
 end
