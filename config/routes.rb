194104beef9a45534daf5238933295752c8787bc
ThomasLaneyrie <thomas.laneyrie@gmail.com>
2023-03-22 11:33:12 +0100

    stripe avec création de order comme dans le projet chaton

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get "static_pages/kitUI"
 
   devise_for :users
-  resources :users, only: [:show, :destroy]
+  resources :users, only: [:show,:edit, :update, :destroy]
   resources :products
 
   # Routes relatives aux product_carts
@@ -11,6 +11,12 @@ Rails.application.routes.draw do
   get 'product_carts/:id/add' => "product_carts#add_quantity", as: "product_cart_add"
   get 'product_carts/:id/reduce' => "product_carts#reduce_quantity", as: "product_cart_reduce"
 
-  # Routes relatives aux carts
-  resources :carts, only: [:show]
+  # Routes relatives aux carts et au payement Stripe
+  resources :carts, only: [:show] do
+    scope '/checkout' do
+      post 'create', to: 'checkout#create', as: 'checkout_create'    
+      get 'success', to: 'checkout#success', as: 'checkout_success'
+      get 'cancel', to: 'checkout#cancel', as: 'checkout_cancel'
+    end
+  end
 end
