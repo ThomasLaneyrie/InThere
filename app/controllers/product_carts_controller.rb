320269e22899eb514144573f7c1f7bb9436eac58
ThomasLaneyrie <thomas.laneyrie@gmail.com>
2023-03-23 12:00:09 +0100

    Turbo_Stream panier E/C non fonctionnel

--- app/controllers/product_carts_controller.rb
+++ app/controllers/product_carts_controller.rb
@@ -9,8 +9,13 @@ class ProductCartsController < ApplicationController
   def add_quantity
     @product_cart = ProductCart.find(params[:id])
     @product_cart.quantity += 1
-    @product_cart.save
-    redirect_to cart_path(@product_cart.cart)
+    # @product_cart.save
+    # redirect_to cart_path(@product_cart.cart)
+    respond_to do |format|
+      if @product_cart.save
+        format.turbo_stream { render turbo_stream: turbo_stream.update('all_product_carts', @product_cart) }
+      end 
+    end
   end
 
   def reduce_quantity
