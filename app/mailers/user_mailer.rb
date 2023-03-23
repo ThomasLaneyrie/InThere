753109fb81333343764c24d3f0c7d811fe1c86e9
ThomasLaneyrie <thomas.laneyrie@gmail.com>
2023-03-23 17:16:54 +0100

    mail welcome user personnalisé

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,16 @@
 class UserMailer < ApplicationMailer
   default from: 'zaritus3@hotmail.com'    
+
   def welcome_email(user)               
     @user = user                                                         
     @url  = 'http://monsite.fr/login'                                    
     mail(to: @user.email, subject: 'Bienvenue chez nous !')            
   end
+
+  def purchase_email(order)
+		@order = order
+		@cart = @order.cart
+		mail(to: @order.user.email, subject: 'Récapitulatif de votre commande')
+	end
+
 end
