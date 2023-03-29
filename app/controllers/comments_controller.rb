fbc1e3a598fc07fb3eadf291f2ecf31e5b95dd6c
Rea <andrea.solisg@gmail.com>
2023-03-28 23:25:15 -0600

    destroy working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -59,7 +59,7 @@ class CommentsController < ApplicationController
     @comment.destroy
 
     respond_to do |format|
-      format.html { redirect_to comments_url, notice: "Comment was successfully destroyed." }
+      format.html { redirect_to products_path, notice: "Comment was successfully destroyed." }
       format.json { head :no_content }
     end
   end
