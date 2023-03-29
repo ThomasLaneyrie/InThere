3b7fe62cf1899836f71f44c9c6a49736349ceca3
ThomasLaneyrie <thomas.laneyrie@gmail.com>
2023-03-29 17:06:02 +0200

    admin mis à jour mais pas fini

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   # Routes relatives aux administrateurs
   namespace :admin do
     root to: "static_pages#dashboard"
-    resources :products, only: [:new, :create, :edit, :update, :destroy]
+    resources :products, only: [:destroy]
   end
 
   # Routes relatives aux produits
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2023_03_29_073204) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_29_122955) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -114,7 +113,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_29_073204) do
   end
 
   create_table "products", force: :cascade do |t|
-    t.text "title"
+    t.string "title"
     t.text "description"
     t.decimal "price", precision: 6, scale: 2
     t.string "image_url"
