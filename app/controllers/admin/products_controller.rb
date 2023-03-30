ee463a1fb1c2009b833000ed069e984844e90953
Rea <andrea.solisg@gmail.com>
2023-03-29 23:47:19 -0600

    pages admin

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -2,6 +2,10 @@ class Admin::ProductsController < ApplicationController
   before_action :authenticate_user!
   before_action :check_if_admin
 
+  def index
+    @products = Product.all
+  end
+
   def destroy
     @product = Product.find(params[:id])
     @product.destroy
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,15 @@
 class Admin::UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :check_if_admin
+
+  def index
+    @users = User.all
+  end 
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to admin_users_path, notice: "User #{user.email} has been deleted."
+  end
+
 end
