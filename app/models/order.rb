05d91643f76ff8eedf57c2279cddda2a5f41add2
Rea <andrea.solisg@gmail.com>
2023-03-30 13:17:00 -0600

    MAIL ADMIN

--- app/models/order.rb
+++ app/models/order.rb
@@ -10,4 +10,7 @@ class Order < ApplicationRecord
   def order_mail
 	  UserMailer.purchase_email(self).deliver_now
   end
+  def admin_order_send
+    AdminMailer.admin_order_send(self).deliver_now
+  end
 end
